dcb77199918653f0caa2589fad1209085d41be70
davidperezjpa <davidperezjpa@gmail.com>
2023-11-28 14:39:16 -0600

    [COMPLETED]: Magic prison wall rune

---
+++ data-otservbr-global/scripts/spells/runes/x-furia/magic_prison_wall.lua
@@ -0,0 +1,23 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
+combat:setParameter(COMBAT_PARAM_CREATEITEM, ITEM_MAGICWALL)
+combat:setParameter(COMBAT_PARAM_EFFECT, 44)
+combat:setArea(createCombatArea(AREA_PRISON_SQUARE1X1))
+
+local rune = Spell("rune")
+function rune.onCastSpell(creature, variant, isHotkey)
+	return combat:execute(creature, variant)
+end
+
+rune:name("Magic Prison Wall Rune")
+rune:group("attack")
+rune:castSound(SOUND_EFFECT_TYPE_SPELL_OR_RUNE)
+rune:impactSound(SOUND_EFFECT_TYPE_SPELL_MAGIC_WALL_RUNE)
+rune:cooldown(2 * 1000)
+rune:groupCooldown(2 * 1000)
+rune:level(1000)
+rune:magicLevel(1)
+rune:runeId(3159)
+rune:charges(0)
+rune:allowFarUse(true)
+rune:register()
