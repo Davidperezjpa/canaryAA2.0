a83e87ef64c4f7ca95098ef0acf80484f4ce3e28
davidperezjpa <davidperezjpa@gmail.com>
2023-11-29 12:28:08 -0600

    [COMPLETED]: Mana rune, super mana rune, sayayin mana rune, ultimate healing rune, super uh rune. Needs formula update

--- data-otservbr-global/scripts/spells/runes/x-furia/super_uh_rune.lua
+++ data-otservbr-global/scripts/spells/runes/x-furia/super_uh_rune.lua
@@ -6,8 +6,14 @@ combat:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, 1)
 combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 3) + (maglevel * 7.3) + 42
-	local max = (level / 3) + (maglevel * 12.4) + 90
+	-- if level < 3000 then
+
+	-- else if level < 7000 then
+
+	-- else
+	local min = (level / 3) + (maglevel * 7.3) + 20000
+	local max = (level / 3) + (maglevel * 12.4) + 20000
+	-- end
 	return min, max
 end
 
@@ -19,22 +25,21 @@ function rune.onCastSpell(creature, var, isHotkey)
 	if Monster(var:getNumber(1073762188)) then
 		creature:sendCancelMessage("Sorry, not possible.")
 		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
-		return false
+	return false
 	else
 		return combat:execute(creature, var)
 	end
 end
 
-rune:id(5)
 rune:group("healing")
-rune:name("ultimate healing rune")
+rune:name("super uh rune")
 rune:castSound(SOUND_EFFECT_TYPE_SPELL_OR_RUNE)
 rune:impactSound(SOUND_EFFECT_TYPE_SPELL_ULTIMATE_HEALING_RUNE)
-rune:runeId(3160)
+rune:runeId(3187)
 rune:allowFarUse(true)
 rune:charges(0)
-rune:level(100)
-rune:magicLevel(0)
+rune:level(3000)
+rune:magicLevel(5)
 rune:cooldown(1 * 1000)
 rune:groupCooldown(1 * 1000)
 rune:isAggressive(false)
