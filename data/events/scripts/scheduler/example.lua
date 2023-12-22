b7c38b471afc8a3a6e44cb0bd7aea0d7fb004b8c
davidperezjpa <davidperezjpa@gmail.com>
2023-12-21 21:43:25 -0600

    [FIX]: Removed global events from tibia rl map

--- data/events/scripts/scheduler/example.lua
+++ data/events/scripts/scheduler/example.lua
@@ -21,5 +21,5 @@ function Example_Two.onThink(interval)
 	return true
 end
 
-Example_Two:interval(10000) -- 10 seconds interval
-Example_Two:register()
+-- Example_Two:interval(10000) -- 10 seconds interval
+-- Example_Two:register()
