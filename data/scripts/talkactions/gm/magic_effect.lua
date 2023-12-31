de50a47fd366197c8f155142ca3b9e5e69079b09
davidperezjpa <davidperezjpa@gmail.com>
2023-12-31 10:39:02 -0600

    [FIX]: Magic effect talkaction timing fix

--- data/scripts/talkactions/gm/magic_effect.lua
+++ data/scripts/talkactions/gm/magic_effect.lua
@@ -20,11 +20,13 @@ function magicEffect.onSay(player, words, param)
 
 	if param == "all" then
 		effectActivated = true
+		local timing = 0
 		for i = 1,250,1 do
 			if (80 < i and i < 158) or (158 < i and i < 167) then
 				goto continue
 			end
-			addEvent(sendMagicEffect, i* 700, player:getId(), i)
+			addEvent(sendMagicEffect, timing* 700, player:getId(), i)
+			timing = timing + 1
 			::continue::
 		end
 		return true
